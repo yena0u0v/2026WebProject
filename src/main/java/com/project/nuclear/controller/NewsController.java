67d2c3a19c033038594b7a68df3f22fe3c23cf56
NANA <yena0u0v@gmail.com>
2026-04-28 13:10:09 +0900

    gradle, controller 수정

--- src/main/java/com/project/nuclear/controller/NewsController.java
+++ src/main/java/com/project/nuclear/controller/NewsController.java
@@ -1,3 +1,14 @@
+package com.project.nuclear.controller;
+
+import com.project.nuclear.service.NewsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class NewsController {
